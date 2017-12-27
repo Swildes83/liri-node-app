0f85f23baeab8f0e6b659601ecd12352a264fdcc
Spencer Wildes <spencerwildes@gmail.com>
2017-12-27 07:41:50 -0800

    final

---
+++ gitignore.txt/keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '9sp98xO6FhNboXfpcAGD1K74v',
+  consumer_secret: 'Xi6ylOoku2rnUUyxYrLKUEQU53fYOYZf1bcUVDLC8RKKvT5u0Q',
+  access_token_key: '945476726309531648-fv3XppsAqVQAcD0VqibdfK60t2lhYNq',
+  access_token_secret: 'xiV407fDNC4xWSEZgHPwYVBvNReFv6h3LCu0VDxDEYiem',
+}
+
+var spotifyKeys = {
+    client_id: "",
+    client_secret: ""
+  }
+  
+module.exports = {
+twitterKeys: twitterKeys,
+spotifyKeys: spotifyKeys
+}
